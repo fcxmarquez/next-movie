1cf49f826cd61e410ed4d62a51540f195d35dba6
fcxmarquez <francisco.marquez.solt@gmail.com>
2022-09-27 13:55:10 -0500

    feat: added lint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="columns is-mobile">
-        <div className="column is-full">
+         <div className="column is-full">
           Hi <a href="">Hello there</a>
         </div>
         <div className="column">hello</div>
